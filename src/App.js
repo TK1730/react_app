23cf6d93ec41ed3169696888bc353ef487f4cb2a
TK1730 <kazuki.iyh@gmail.com>
2025-01-28 20:15:18 +0900

    画像描画

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https//i.imgur.com/yX0vd0Ss.jpg',
+  imageSize: 90,
+}
+
 function MyButton() {
   return (
     <button>I'm a button</button>
   )
 }
 
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+        />
+    </>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
       <MyButton />
+      <Profile />
     </div>
   );
 }
